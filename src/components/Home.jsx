41c8257d83c3a15e0cda2c39b026c9ff95a16676
Abraham L. Infante <abraham.leguizamon@hotmail.com>
2023-03-23 13:53:50 +0100

    added own room glb file for render
    added own room glb file for render

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { motion } from 'framer-motion';
 import { styles } from '../styles';
-import { ComputersCanvas } from './canvas';
+import { RoomCanvas } from './canvas';
 
 const Home = () => {
   return (
@@ -17,7 +17,7 @@ const Home = () => {
             I'm a <span className="text-[#915eff]">software engineer</span> developing <br className="sm:block hidden"/> new automations, animations and webpages</p>
         </div>
       </div>
-      <ComputersCanvas/>
+      <RoomCanvas/>
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
